0ab53e0351d078edae48aa81dc52eedd6bc9b636
Minha Shameer <minha142007@gmail.com>
2025-06-25 10:24:36 +0400

    Update 01_matrix_multiplication.py
    Added time complexity description

--- 01_matrix_multiplication.py
+++ 01_matrix_multiplication.py
@@ -11,11 +11,10 @@ def multiply_matrix(A,B):
     p=len(B[0])													#p = number of columns in B
     product= [[0 for _ in range(p)] for _ in range(m)]			#creates a matrix with m rows and p columns filled with 0
     
-    for i in range(m):											#loops over rows of A
-        for j in range(p):										#loops over columns of B
-            for k in range(n):									#loops over elements in row of A and column of B
+    for i in range(m):						#m+1					#loops over rows of A
+        for j in range(p):					#m(p+1)					#loops over columns of B
+            for k in range(n):				#m*p(n+1)					#loops over elements in row of A and column of B
                 product[i][j]+= A[i][k] *B[k][j]
-    
     print (product)
-
+    #Time Complexity = m+1+m(p+1)+m*p(n+1)=n+1+n(n+1)+n^2(n+1)=n+1+n^2+n^3+n=O(n^3)
 multiply_matrix([[1,2],[3,4]],[[6,8,10],[7,9,11]])				#example 
